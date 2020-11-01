8190bedc0c5bad000d2810a5ca36d4f98bea5010
XDS <554536080@qq.com>
2020-11-01 17:28:32 +0800

    Initial commit

---
+++ app/src/main/java/com/kuma/kangaroof/MainActivity.java
@@ -0,0 +1,20 @@
+package com.kuma.kangaroof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.main_web_view);
+
+        webView.loadUrl("file:///android_asset/main_h5.html");
+    }
+}
